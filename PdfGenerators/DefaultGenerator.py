2005be7a3201eec8f192260d08ab1d07c80a7e94
Jack Wainwright <sc14jw@leeds.ac.uk>
2016-10-09 11:06:02 +0100

    Created DefaultPdfGenerator

--- PdfGenerators/DefaultGenerator.py
+++ PdfGenerators/DefaultGenerator.py
@@ -1,8 +1,35 @@
-import PdfGenerator.PdfGenerator
+import sys
+sys.path.append(".")
+
+from PdfGenerators.PdfGenerator import PdfGenerator
+from weasyprint import HTML, CSS
 
 class DefaultGenerator(PdfGenerator):
 
-    def generatePdf(filename, text):
+    def __init__(self):
+        self.styleSheet = None
 
-        if (not filename is str) or (not text is str):
+    def generatePdf(self,filename, text):
+
+        if (not isinstance(filename,str)) or (not isinstance(text,str)):
             raise AttributeError("either filename or text is not a string")
+
+        if not self.styleSheet:
+            HTML(string=text).write_pdf(filename + ".pdf")
+
+        else:
+            HTML(string=text).write_pdf(filename + ".pdf", stylesheets=[self.styleSheet])
+
+
+    def addStylesheet(self,filename):
+
+        if (not filename is str):
+            raise AttributeError("filename must be a string")
+
+        self.styleSheet = CSS(filename=filename + ".css")
+
+
+if __name__ == '__main__':
+
+    generator = DefaultGenerator()
+    generator.generatePdf("test", "<p> this is a test </p>")
--- PdfGenerators/PdfGenerator.py
+++ PdfGenerators/PdfGenerator.py
@@ -1,4 +1,8 @@
 class PdfGenerator:
-    def generatePdf(filename,text):
+
+    def __init__(self):
+        pass
+
+    def generatePdf(self,filename,text):
 
         raise NotImplemented("Please use an extension of PdfGenerator")
