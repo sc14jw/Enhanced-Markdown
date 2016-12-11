d6a6a4b676faabb8ff94caa7e295342d3a6a4747
Jack Wainwright <sc14jw@leeds.ac.uk>
2016-12-11 10:31:07 +0000

    completed initial table module

--- Modules/TableModule.py
+++ Modules/TableModule.py
@@ -3,12 +3,14 @@ from Modules.Module import Module
 class TableModule(Module):
     ''' Module to deal with the creation of tables '''
 
-    self.commands = {"table": self.createTable, "row": self.row, "column": self.column, "closeTable": self.closeTable}
+    self.commands = {"table": self.createTable, "row": self.row, "column": self.column, "closeTable": self.closeTable, "section" : self.section}
 
     def __init__(self):
 
         self.openRow = False
         self.openCol = False
+        self.openBody = False
+        self.openHead = False
 
     def completeCommand(self, command):
 
@@ -22,7 +24,8 @@ class TableModule(Module):
     def getCommands(self):
         return {"table": "create a new table - can optionally use [<id>]",
                 "closeTable": "close an opened tabled",
-                "row": "create a new row"}
+                "row": "create a new row or close a currently open row",
+                "column": "create a new column or close a currently open column - can optionally use [<colSpan>, <rowSpan>]"}
 
 
     def createTable(self):
@@ -31,6 +34,9 @@ class TableModule(Module):
         if '[' in self.command:
 
             return "<table id=" + self.command[self.command.find('['):self.command.find(']') - 1] + ">"
+
+        return "</table>"
+
 
     def closeTable(self):
         ''' close a table '''
@@ -41,9 +47,53 @@ class TableModule(Module):
         ''' create/close a row '''
 
         if self.openRow:
+
             self.openRow = False
             return "</tr>"
 
         return "<tr>"
 
+
     def column(self):
+        ''' create/close a column '''
+
+        if self.openCol:
+
+            self.openCol = False
+            return "</td>"
+
+        if '[' in self.command;
+
+            parameters = self.command[self.command.find('['): self.command.find(']') - 1]
+            parameters = parameters.split(',')
+
+            if parameters.size() == 1:
+                return "<td colspan=" + parameters[0] + ">"
+
+
+            return "td colspan=" + parameters[0] + "rowspan=" + parameters[1] + ">"
+
+
+    def section(self):
+        ''' create/close a given section '''
+
+        parameter = self.command[self.command.find('(') : self.command.find(')') - 1]
+
+        if not (parameter == "body" or parameter == "head"):
+            raise AttributeError(parameter + "not supported as a section. Please use either head or body")
+
+        if parameter == "body":
+
+            if self.bodyOpen:
+
+                self.bodyOpen = False
+                return "</tbody>"
+
+            return "<tbody>"
+
+        if self.headOpen:
+
+            self.headOpen = False
+            return "</thead>"
+
+        return "<thead>"
