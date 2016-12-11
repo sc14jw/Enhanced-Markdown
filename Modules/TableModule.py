76b8c131ca2725cf838784db6524619e60d4ad70
Jack Wainwright <sc14jw@leeds.ac.uk>
2016-12-11 10:46:11 +0000

    added support for table headers to the TableModule

--- Modules/TableModule.py
+++ Modules/TableModule.py
@@ -3,14 +3,16 @@ from Modules.Module import Module
 class TableModule(Module):
     ''' Module to deal with the creation of tables '''
 
-    self.commands = {"table": self.createTable, "row": self.row, "column": self.column, "closeTable": self.closeTable, "section" : self.section}
-
     def __init__(self):
+
+        self.commands = {"table": self.createTable, "row": self.row, "column": self.column, "section" : self.section, "header" : self.header}
 
         self.openRow = False
         self.openCol = False
-        self.openBody = False
-        self.openHead = False
+        self.bodyOpen = False
+        self.headOpen = False
+        self.tableOpen = False
+        self.headerOpen = False
 
     def completeCommand(self, command):
 
@@ -22,26 +24,27 @@ class TableModule(Module):
 
 
     def getCommands(self):
-        return {"table": "create a new table - can optionally use [<id>]",
-                "closeTable": "close an opened tabled",
+        return {"table": "create a new table or close a currently open table - can optionally use [<id>]",
                 "row": "create a new row or close a currently open row",
-                "column": "create a new column or close a currently open column - can optionally use [<colSpan>, <rowSpan>]"}
+                "column": "create a new column or close a currently open column - can optionally use [<colSpan>, <rowSpan>]",
+                "section": "create a new section or close a currently open section - requires the parameter head or body relating to the section to interact with"
+                "header": "create a new column header or close a currently open header"}
 
 
     def createTable(self):
         ''' create a new table '''
 
+        if self.tableOpen:
+
+            self.tableOpen = False
+            return "</table>"
+
         if '[' in self.command:
 
-            return "<table id=" + self.command[self.command.find('['):self.command.find(']') - 1] + ">"
+            return "<table id=" + self.command[self.command.find('[') + 1 :self.command.find(']')] + ">"
 
-        return "</table>"
+        return "<table>"
 
-
-    def closeTable(self):
-        ''' close a table '''
-
-        return "</table>"
 
     def row(self):
         ''' create/close a row '''
@@ -62,25 +65,28 @@ class TableModule(Module):
             self.openCol = False
             return "</td>"
 
-        if '[' in self.command;
+        if '[' in self.command:
 
-            parameters = self.command[self.command.find('['): self.command.find(']') - 1]
+            parameters = self.command[self.command.find('[') + 1: self.command.find(']')]
             parameters = parameters.split(',')
 
             if parameters.size() == 1:
                 return "<td colspan=" + parameters[0] + ">"
 
 
-            return "td colspan=" + parameters[0] + "rowspan=" + parameters[1] + ">"
+            return "<td colspan=" + parameters[0] + "rowspan=" + parameters[1] + ">"
 
+        return "<td>"
 
     def section(self):
         ''' create/close a given section '''
 
-        parameter = self.command[self.command.find('(') : self.command.find(')') - 1]
+        parameter = self.command[self.command.find('(') + 1 : self.command.find(')')]
+
+        print("parameter = " + parameter)
 
         if not (parameter == "body" or parameter == "head"):
-            raise AttributeError(parameter + "not supported as a section. Please use either head or body")
+            raise AttributeError(parameter + " not supported as a section. Please use either head or body")
 
         if parameter == "body":
 
@@ -97,3 +103,12 @@ class TableModule(Module):
             return "</thead>"
 
         return "<thead>"
+
+    def header(self):
+
+        if self.headerOpen:
+
+            self.headerOpen = False
+            return "</th>"
+
+        return "<th>"
